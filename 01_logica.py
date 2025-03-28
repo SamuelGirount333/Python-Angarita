fca22df5cedac3450ea285aba097ce26e5041417
Samuel Gomez <samuelgozgiraldo1357@gmail.com>
2025-03-28 00:58:44 -0500

    Se agrega material de trabajo de python de logica

---
+++ 01_logica.py
@@ -0,0 +1,12 @@
+
+def two_values(value_a, value_b):
+    if value_a > value_b:
+        print(value_a)
+    else:
+        print(value_b)
+    
+    resultado = two_values
+    return resultado
+
+two_values(12,33)
+
